5228e6166fa509b646121c8ed1963f175f951f40
Andrey Stoev <andrey.stoev@mimacom.com>
2020-12-23 21:19:45 +0100

    - renamed serviceApp

--- onion-arch-service/src/main/java/com/andistoev/onionarchservice/OnionArchServiceApplication.java
+++ onion-arch-service/src/main/java/com/andistoev/onionarchservice/OnionArchServiceApplication.java
@@ -7,10 +7,10 @@ import org.springframework.core.env.Environment;
 
 @Slf4j
 @SpringBootApplication
-public class OnionArchApplication {
+public class OnionArchServiceApplication {
 
 	public static void main(String[] args) {
-        Environment env = SpringApplication.run(OnionArchApplication.class, args).getEnvironment();
+        Environment env = SpringApplication.run(OnionArchServiceApplication.class, args).getEnvironment();
 		log.info("Start SwaggerUI to use the Shopping List's APIs: http://localhost:{}/swagger-ui/index.html#/shopping-list-controller", env.getProperty("server.port"));
 	}
 
--- onion-arch-service/src/test/java/com/andistoev/onionarchservice/OnionArchServiceApplicationTest.java
+++ onion-arch-service/src/test/java/com/andistoev/onionarchservice/OnionArchServiceApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OnionArchApplicationTest {
+class OnionArchServiceApplicationTest {
 
     @Test
     void contextLoads() {
