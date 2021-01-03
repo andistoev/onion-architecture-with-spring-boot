fa43e112fe0dd5a8ea505604807f337f3ff91da1
Andrey Stoev <andrey.stoev@mimacom.com>
2021-01-03 21:32:48 +0100

    - fixed apis: added ok() and created(...), made uri without -, reused BASE_URI

--- onion-arch-service-infra-api-rest/src/main/java/com/andistoev/onionarchservice/infra/api/rest/ShoppingListController.java
+++ onion-arch-service-infra-api-rest/src/main/java/com/andistoev/onionarchservice/infra/api/rest/ShoppingListController.java
@@ -5,34 +5,45 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.Validate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Arrays;
 import java.util.UUID;
 
+import static com.andistoev.onionarchservice.infra.api.rest.ShoppingListController.BASE_URI;
+import static org.springframework.http.ResponseEntity.created;
+import static org.springframework.http.ResponseEntity.ok;
+
 @RestController
-@RequestMapping("/api/1/shopping-lists")
+@RequestMapping(BASE_URI)
 @Api(value = "API to shopping list", produces = "application/json")
 @RequiredArgsConstructor
 public class ShoppingListController {
+
+    protected static final String BASE_URI = "/api/v1/shoppinglists";
 
     private final ShoppingListService shoppingListService;
 
     @ApiOperation(value = "Create new shopping list", produces = "application/json")
     @PostMapping("/")
-    UUID createShoppingList() {
-        return shoppingListService.createShoppingList();
+    ResponseEntity<?> createShoppingList(UriComponentsBuilder uriComponentsBuilder) {
+        UriComponents uriComponents = uriComponentsBuilder.path(BASE_URI + "/{shoppingListId}")
+            .buildAndExpand(shoppingListService.createShoppingList());
+        return created(uriComponents.toUri()).build();
     }
 
     @ApiOperation(value = "Add new item to a shopping list", produces = "application/json")
     @PostMapping("/{shoppingListId}/items")
-    UUID addItemToTheShoppingList(
+    ResponseEntity<?> addItemToTheShoppingList(
+        UriComponentsBuilder uriComponentsBuilder,
         @PathVariable UUID shoppingListId,
         @RequestParam String productName,
         @RequestParam Double price,
@@ -43,13 +54,15 @@ public class ShoppingListController {
 
         Validate.isTrue(quantity >= 1, "The quantity has to be greater or equal to 1");
 
-        return shoppingListService.addItemToTheShoppingList(shoppingListId, productName, price, quantity);
+        UriComponents uriComponents = uriComponentsBuilder.path(BASE_URI + "/" + shoppingListId + "/items/{itemId}")
+            .buildAndExpand(shoppingListService.addItemToTheShoppingList(shoppingListId, productName, price, quantity));
+        return created(uriComponents.toUri()).build();
     }
 
     @ApiOperation(value = "Get shopping list's total price, with the shipping costs of 10 credits included (if the price is >=100 credits, then the shipping is free of charge)!", produces = "application/json")
-    @GetMapping("/{shoppingListId}/total-price")
-    double getTotalPrice(@PathVariable UUID shoppingListId) {
+    @GetMapping("/{shoppingListId}/totalprice")
+    ResponseEntity<?> getTotalPrice(@PathVariable UUID shoppingListId) {
         Validate.notNull(shoppingListId, "Missing mandatory input parameter: shoppingListId");
-        return shoppingListService.getTotalPrice(shoppingListId);
+        return ok().body(shoppingListService.getTotalPrice(shoppingListId));
     }
 }
--- onion-arch-service-infra-api-rest/src/test/java/com/andistoev/onionarchservice/infra/api/rest/ShoppingListControllerTest.java
+++ onion-arch-service-infra-api-rest/src/test/java/com/andistoev/onionarchservice/infra/api/rest/ShoppingListControllerTest.java
@@ -7,14 +7,13 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static com.andistoev.onionarchservice.infra.api.rest.ShoppingListController.BASE_URI;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ShoppingListController.class)
 class ShoppingListControllerTest {
-
-    private static final String API_URI = "/api/1/shopping-lists";
 
     private static final UUID MOCK_SHOPPING_LIST_ID = UUID.randomUUID();
 
@@ -23,25 +22,25 @@ class ShoppingListControllerTest {
 
     @Test
     void createShoppingList() throws Exception {
-        mockMvc.perform(post(API_URI + "/"))
-            .andExpect(status().isOk());
+        mockMvc.perform(post(BASE_URI + "/"))
+            .andExpect(status().isCreated());
     }
 
     @Test
     void addItemToTheShoppingList() throws Exception {
-        String uri = String.format("%s/%s/items", API_URI, MOCK_SHOPPING_LIST_ID);
+        String uri = String.format("%s/%s/items", BASE_URI, MOCK_SHOPPING_LIST_ID);
         mockMvc
             .perform(post(uri)
                 .queryParam("productName", "milk")
                 .queryParam("price", "3.8")
                 .queryParam("quantity", "1")
             )
-            .andExpect(status().isOk());
+            .andExpect(status().isCreated());
     }
 
     @Test
     void getTotalPrice() throws Exception {
-        String uri = String.format("%s/%s/total-price", API_URI, MOCK_SHOPPING_LIST_ID);
+        String uri = String.format("%s/%s/totalprice", BASE_URI, MOCK_SHOPPING_LIST_ID);
         mockMvc.perform(get(uri))
             .andExpect(status().isOk());
     }
